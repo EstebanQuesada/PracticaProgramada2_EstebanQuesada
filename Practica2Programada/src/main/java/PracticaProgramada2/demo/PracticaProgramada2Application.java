cbb06cce62128586d1a9131fa228e03042121fc4
EstebanQuesada <equesada00666@ufide.ac.cr>
2024-02-13 00:59:29 -0600

    PracticaProgramada2

---
+++ Practica2Programada/src/main/java/PracticaProgramada2/demo/PracticaProgramada2Application.java
@@ -0,0 +1,13 @@
+package PracticaProgramada2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaProgramada2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaProgramada2Application.class, args);
+	}
+
+}
---
+++ Practica2Programada/src/test/java/PracticaProgramada2/demo/PracticaProgramada2ApplicationTests.java
@@ -0,0 +1,13 @@
+package PracticaProgramada2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaProgramada2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
